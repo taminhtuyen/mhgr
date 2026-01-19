8a61cf33ffda274489b5a03346b4d0baaad1b028
Administrator <taminhtuyen@hotmail.com>
2026-01-20 03:51:03 +0700

    cap nhat cac file modal tu dong bang script v5

---
+++ app/Models/FlashSaleProduct.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class FlashSaleProduct extends Model
+{
+    use HasFactory;
+
+    protected $table = 'flash_sale_products';
+
+    protected $guarded = ['id']; // Cho phép điền mọi cột trừ ID (tiện lợi hơn $fillable khi chưa biết cột)
+
+    // TODO: Định nghĩa quan hệ (Relationships) nếu cần thiết
+}
---
+++ app/Models/Wallet.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Wallet extends Model
+{
+    use HasFactory;
+
+    protected $table = 'wallets';
+
+    protected $guarded = ['id']; // Cho phép điền mọi cột trừ ID (tiện lợi hơn $fillable khi chưa biết cột)
+
+    // TODO: Định nghĩa quan hệ (Relationships) nếu cần thiết
+}
